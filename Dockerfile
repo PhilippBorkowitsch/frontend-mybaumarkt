38ca89018f36945303c6a8b80d1ea3826b857637
PhilippBorkowitsch <pborkowitsch@gmail.com>
2021-08-10 20:27:48 +0200

    Ok last try today

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM node:14.17.1-alpine
 WORKDIR /app
 
 ENV PATH /app/node_modules/.bin:$PATH
+ENV PUBLIC_URL https://aims.ctc.ezmeral.de/frontend
 
 COPY package.json ./
 COPY package-lock.json ./
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={'frontend'}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
